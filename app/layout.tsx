232f778a3b9a1c498e902065dfe134a0850ac5c7
jiyoung <cwd05011@gmail.com>
2024-10-08 10:16:26 +0900

    fix: local font 경로 이동 및 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,23 @@ import ModalProvider from './_provider/modal-provider';
 const primaryFont = localFont({
   src: [
     {
-      path: './fonts/Pretendard-Light.woff2',
+      path: '../public/fonts/Pretendard-Light.woff2',
       weight: '300',
     },
     {
-      path: './fonts/Pretendard-Regular.woff2',
+      path: '../public/fonts/Pretendard-Regular.woff2',
       weight: '400',
     },
     {
-      path: './fonts/Pretendard-Medium.woff2',
+      path: '../public/fonts/Pretendard-Medium.woff2',
       weight: '500',
     },
     {
-      path: './fonts/Pretendard-Semibold.woff2',
+      path: '../public/fonts/Pretendard-Semibold.woff2',
       weight: '600',
     },
     {
-      path: './fonts/Pretendard-Bold.woff2',
+      path: '../public/fonts/Pretendard-Bold.woff2',
       weight: '700',
     },
   ],
